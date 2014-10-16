40f99d412782a7baaa660314db932d940c62efdd
Jon Willesen <jonw@instructure.com>
2014-10-16 17:52:54 -0600

    change the line and beat of a card
    for now, you have to be careful not to choose a slot that's
    already taken.

--- app/web_modules/cardDialog.jsx
+++ app/web_modules/cardDialog.jsx
@@ -8,6 +8,8 @@ var _ = require('lodash');
 var RBS = require('react-bootstrap');
 var Button = RBS.Button;
 var ButtonToolbar = RBS.ButtonToolbar;
+var DropdownButton = RBS.DropdownButton;
+var MenuItem = RBS.MenuItem;
 
 var WholeBoardStore = require('wholeBoardStore');
 var CardTitleEditor = require('cardTitleEditor');
@@ -39,8 +41,8 @@ var CardDialog = React.createClass({
       }, this);
     } else {
       initialCard = {
-        beat_id: nextProps.params.beatId,
-        line_id: nextProps.params.lineId,
+        beat_id: parseInt(nextProps.params.beatId),
+        line_id: parseInt(nextProps.params.lineId),
         title: 'New Card',
         description: '',
       };
@@ -105,14 +107,28 @@ var CardDialog = React.createClass({
     return WholeBoardStore.saveCard(this.state.editedCard);
   },
 
+  getCurrentBeat: function() {
+    return _.find(this.props.beats, function(beat) {
+      return beat.id === this.state.editedCard.beat_id;
+    }.bind(this));
+  },
+
+  getCurrentLine: function() {
+    return _.find(this.props.lines, function(line) {
+      return line.id === this.state.editedCard.line_id;
+    }.bind(this));
+  },
+
   renderButtonBar: function() {
     if (this.state.editedCard.id) {
       return (
-        <Button className="card-dialog__close" 
-          bsStyle="primary"
-          onClick={this.closeModal}>
-          Close
-        </Button>
+        <ButtonToolbar className="card-dialog__button-bar">
+          <Button className="card-dialog__close"
+            bsStyle="primary"
+            onClick={this.closeModal}>
+            Close
+          </Button>
+        </ButtonToolbar>
       );
     } else {
       return (
@@ -130,10 +146,57 @@ var CardDialog = React.createClass({
     }
   },
 
+  renderBeatItems: function() {
+    return this.props.beats.reduce(function(elts, beat) {
+      elts.push(
+        <MenuItem
+          key={beat.id}
+          onSelect={this.setEditedCardState.bind(this, {beat_id: beat.id})}
+          >
+          {beat.title}
+        </MenuItem>
+      );
+      return elts;
+    }.bind(this), []);
+  },
+
+  renderLineItems: function() {
+    return this.props.lines.reduce(function(elts, line) {
+      elts.push(
+        <MenuItem
+          key={line.id}
+          onSelect={this.setEditedCardState.bind(this, {line_id: line.id})}
+          >
+          {line.title}
+        </MenuItem>
+      );
+      return elts;
+    }.bind(this), []);
+  },
+
   render: function() {
+    var ids = {
+      beat: _.uniqueId("select-beat-"),
+      line: _.uniqueId("select-line-"),
+    };
+
     return (
       <Modal isOpen={true} onRequestClose={this.closeModal}>
         <div className="card-dialog">
+          <div className="card-dialog__header">
+            <div className="card-dialog__line">
+              <label className="card-dialog__line-label" htmlFor={ids.line}>Select Line</label>
+              <DropdownButton id={ids.line} className="card-dialog__select-line" title={this.getCurrentLine().title}>
+                {this.renderLineItems()}
+              </DropdownButton>
+            </div>
+            <div className="card-dialog__beat">
+              <label className="card-dialog__beat-label" htmlFor={ids.beat}>Select Beat</label>
+              <DropdownButton id={ids.beat} className="card-dialog__select-beat" title={this.getCurrentBeat().title}>
+                {this.renderBeatItems()}
+              </DropdownButton>
+            </div>
+          </div>
           <div className="card-dialog__title">
             <CardTitleEditor card={this.state.editedCard}
               isOpen={this.isTitleOpen()}
